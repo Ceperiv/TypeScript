a7ab458d8344e9c1ee5c9a0b1d4b4defd3f43584
Andriy Konoval <ceperiv@dmail.com>
2022-07-26 16:04:10 +0300

    master

--- enum/my.enum.ts
+++ enum/my.enum.ts
@@ -1,6 +1,3 @@
 export enum MyEnum{
-    OK = 200,
-    BAD_REQUEST =400,
-        NOT_FOUND =404,
 
 }
--- interface/interface.ts
+++ interface/interface.ts
@@ -1,21 +1,6 @@
 interface IUser {
 
-    id: number
-    name: string
-    username: string
-    email: string
-    address: {
-        street: string
-        suite: string
-        city: string
-        zipcode: number
-        geo: {
-            lat: number
-            lng: number
-        }
-    }
-
-};
+}
 
 export {
     IUser
